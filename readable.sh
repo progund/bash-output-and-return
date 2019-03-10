b76aa59e7023d9a413bd770c7f6a77022743fa10
Rikard Fröberg <rikard@morus.se>
2019-03-10 02:16:29 +0100

    Some quoting

--- readable.sh
+++ readable.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-for file in $(ls)
+for file in ./*
 do
     # One way of doing the test
     #    test -r $file
     #    if [ $? -eq 0 ]
     # ... and a nicer one
-    if [ -r $file ]
+    if [ -r "$file" ]
     then
         echo "Can read:   $file"
     else
         echo "Can't read: $file"
     fi
+    # Another way:
+    # [[ -r "file" ]] && echo "Can read $file" || echo "Can't read $file"
 done
